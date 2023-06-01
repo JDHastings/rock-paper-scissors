17772ea27cd44de10114591b8de6fbd68d96b9be
Jack Hastings <32724063+JDHastings@users.noreply.github.com>
2023-06-01 16:07:41 -0400

    Added Buttons to RPS!
    -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
    
    Used JS to put add button functionality and increased the user interface
    experience.
    
    -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let computerScore = 0;
+let playerScore = 0;
+let tieCount = 0;
+
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     if(num == 0){
@@ -9,39 +13,66 @@ function getComputerChoice(){
     }
 }
 
-function playRound(){
-    let player = getPlayerChoice();
+function playRound(player){
     let computer = getComputerChoice();
+
+    if(!playerScore && !computerScore && !tieCount){
+        gameEndMessage.remove();
+    }
+
     if(player == computer){
-        return "tie";
+        roundOver(2);
     }else if(player == "scissors" && computer == "rock"){
-        return "Computer Wins!";
+        roundOver(1);
     }else if(player == "scissors" && computer == "paper"){
-        return "You Win!";
+        roundOver(0);
     }else if(player == "paper" && computer == "rock"){
-        return "You Win!";
+        roundOver(0);
     }else if(player == "paper" && computer == "scissors"){
-        return "Computer Wins!";
+        roundOver(1);
     }else if(player == "rock" && computer == "paper"){
-        return "Computer Wins!";
+        roundOver(1);
     }else if(player == "rock" && computer == "scissors"){
-        return "You Win!";
+        roundOver(0);
     }
 }
 
-function getPlayerChoice(){
-    let tempChoice = prompt("Input Rock, Paper, or Scissors").toLowerCase();
-    if(!(tempChoice == "rock" || tempChoice == "paper" || tempChoice == "scissors")){
-        do{
-           tempChoice = prompt("Despicable choice, input rock, paper, or scissors").toLowerCase();
-        }while(!(tempChoice == "rock" || tempChoice == "paper" || tempChoice == "scissors"));
+const cssPlayerScore = document.querySelector('.player-score');
+const cssComputerScore = document.querySelector('.computer-score');
+const cssTieCount = document.querySelector('.tie-count');
+
+function roundOver(victor){
+    if(victor == 0){
+        playerScore++;
+    }else if(victor == 1){
+        computerScore++;
+    }else{
+        tieCount++;
     }
-    return tempChoice;
+    cssPlayerScore.textContent = playerScore;
+    cssComputerScore.textContent = computerScore;
+    cssTieCount.textContent = tieCount;
+    if(playerScore == 5 || computerScore == 5){
+        gameOver(victor);
+        playerScore = 0;
+        computerScore = 0;
+        tieCount = 0;
+    }
 }
 
+const container = document.querySelector('.container');
+const gameEndMessage = document.createElement('div');
 
-alert(playRound());
-alert(playRound());
-alert(playRound());
-alert(playRound());
-alert(playRound());
+function gameOver(victor){
+    if(victor){
+        gameEndMessage.textContent = 'Computer Wins!';
+    }else{
+        gameEndMessage.textContent = 'Player Wins!';
+    }
+    container.append(gameEndMessage);
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', () => {
+    playRound(button.id);
+}));
